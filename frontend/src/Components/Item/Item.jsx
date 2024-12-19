bbebe79bff09077b8a16914462d72bceff3dc19f
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-12-19 15:20:46 +0530

    Updated item card

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Item = (props) => {
+    
   return (
     <>
-        <div className="item flrx flex-col w-64 h-96  bg-gradient-to-tl from-white to-pink-200 p-2 rounded-xl hover:scale-105 transition-all duration-300 cursor-pointer">
+         <Link to={`/product/${props.id}`}><div  className="item flrx flex-col w-64 h-96  bg-gradient-to-tl from-white to-pink-200 p-2 rounded-xl hover:scale-105 transition-all duration-300 cursor-pointer">
             <img className='w-full h-[75%] object-cover rounded-lg' src={props.img} alt="" />
             <p className='text-pink-900'>{props.name}</p>
             <div className='item-price flex items-center gap-5 '>
@@ -15,6 +17,7 @@ const Item = (props) => {
                 </div>
             </div>
         </div>
+        </Link>
     </>
   )
 }
