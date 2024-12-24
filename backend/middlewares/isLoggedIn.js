2f36e3ce88c0e17fdc1dc347f8cf873b01e8a331
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-12-24 08:45:45 +0530

    function to authenticate user

---
+++ backend/middlewares/isLoggedIn.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+module.exports.isLoggedIn = (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'You are not logged in'
+            });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            status: 'fail',
+            message: 'You are not logged in'
+        });
+    }
+}
