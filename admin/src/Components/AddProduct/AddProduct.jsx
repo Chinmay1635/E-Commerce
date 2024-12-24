01add9dd4d7df063ad350169952ae5b9d8370527
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-12-24 08:40:19 +0530

    Added admin pages

---
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import upload_icon from '../../assets/upload_area.svg'
+import { ToastContainer, toast } from 'react-toastify';
+
+const AddProduct = () => {
+
+    const notify = (message) => {
+        toast.success(message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            
+            });
+        
+    };
+    
+
+    const [image, setImage] = useState(false);
+    const imageHandler = (e) => {
+        setImage(e.target.files[0]);
+    }
+
+    const [productDetails, setProductDetails] = useState({
+        name:'',
+        image:'',
+        new_price:'',
+        old_price:'',
+        category:'women'
+    });
+
+    const changeHandler = (e) => {
+        setProductDetails({
+            ...productDetails,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const Add_Product = async () => {
+        let responseData;
+        let product = productDetails;
+        let formData = new FormData();
+        formData.append('product', image);
+
+        await fetch('http://localhost:3000/api/product/addImage' , {
+            method: 'POST',
+            headers:{
+                Accept: 'application/json'
+            },
+            body: formData,
+        }).then((response) => response.json())
+        .then((data)=>responseData=data);
+
+        if(responseData.success){
+            product.image = responseData.image_url;
+            await fetch('http://localhost:3000/api/product/addProduct' , {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(product)
+            }).then((response) => response.json())
+            .then((data)=>{notify(data.message)});
+        }
+    }
+
+  return(
+    <div className='flex items-center gap-8 flex-col w-full h-full py-16'>
+        <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+
+/>
+        <h1 className='text-3xl font-bold'>Add Product</h1>
+        <div>
+            <form className='flex flex-col gap-4 w-96'>
+                <input value={productDetails.name} onChange={changeHandler} className='border-2 border-gray-200 rounded-lg p-2' type="text" name='name' placeholder='Product Name' />
+                <input value={productDetails.new_price} onChange={changeHandler} className='border-2 border-gray-200 rounded-lg p-2' type="number" name='new_price' id='new_price' placeholder='Product Offer Price' />
+                <input value={productDetails.old_price} onChange={changeHandler} className='border-2 border-gray-200 rounded-lg p-2' type="number" name='old_price' id='old_price' placeholder='Product Original Price' />
+                <p className=''>
+                    <label className='border-2 border-gray-200 rounded-lg p-2 bg-white mr-2' htmlFor="category">Category: </label>
+                    <select value={productDetails.category} onChange={changeHandler} name="category" id="category" className='border-2 border-gray-200 rounded-lg p-2 bg-white mr-2'>
+                    <option value="women">women</option>
+                    <option value="men">men</option>
+                    <option value="kid">kid</option>
+                </select>
+                </p>
+
+                <label htmlFor="image" className='cursor-pointer'>
+                    <img className='w-24' src={image ? URL.createObjectURL(image) : upload_icon } alt="" />
+                </label>
+                <input  hidden onChange={imageHandler} className='border-2 border-gray-200 rounded-lg p-2' type="file" name='image' id='image' />
+
+            </form>
+                <button onClick={()=> Add_Product()} className='bg-blue-400 mt-4 text-white rounded-lg p-2'>Add Product</button>
+        </div>
+    </div>
+  )
+}
+
+export default AddProduct
---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+
+const ListProduct = () => {
+
+  const [all_product, setAllProduct] = useState([]);
+  let responseData;
+  const fetchProducts = async () => {
+    await fetch('http://localhost:3000/api/product/all')
+    .then((response) => response.json())
+    .then((data) => responseData = data);
+    setAllProduct(responseData.products);
+  }
+  
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
+  const removeProduct = async (id) => {
+    await fetch('http://localhost:3000/api/product/removeProduct', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id })
+    });
+    fetchProducts();
+  }
+  return (
+    <>
+      <div className='flex flex-col p-8'>
+      <h1 className='text-3xl font-bold'>All Products</h1>
+        <div className="lg:flex w-screen gap-56 p-8 hidden">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Offer Price</p>
+            <p>Price</p>
+            
+            <p>Remove</p>
+        </div>
+        <hr />
+        <div  className='flex p-2 gap-5 flex-col w-screen'>
+            
+            {all_product.map((item, i)=>{
+                
+                    return(
+                        <div key={i}>
+                            <div className="flex flex-col lg:flex-row gap-5 lg:gap-44 sm:p-3 lg:py-3 rounded-lg  w-full  items-center">
+                            <img src={item.image} alt="" className='rounded-md w-2/3 lg:w-[7%] '/>
+                            <p className='text-lg  w-full lg:w-[10%]'>{item.name}</p>
+                            <p className='text-xl text-green-500'>${item.new_price}</p>
+                            <p className='text-xl text-red-500'>${item.old_price}</p>
+                            <button className='flex items-center px-4 py-2 bg-red-500 justify-center text-white rounded-lg' onClick={()=>{removeProduct(item.id)}}>
+                            Remove
+                            </button>
+                         </div>
+                         <hr className='h-[2px] bg-zinc-300 border-none'/>
+                        </div>
+                    )
+                
+            })}    
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ListProduct
