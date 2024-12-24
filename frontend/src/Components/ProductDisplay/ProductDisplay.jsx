3a4f60efaf56619b18d16a14176ddc11ec70a073
Chinmay1635 <chinmaykulkarni165@gmail.com>
2024-12-24 08:48:25 +0530

    added error and success displaying messages with authentication

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,16 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { Link, Links } from 'react-router-dom'
 import star_icon from '../Assets/Frontend_Assets/star_icon.png'
 import star_dull_icon from '../Assets/Frontend_Assets/star_dull_icon.png'
 import { ShopContext } from '../../Context/ShopContext'
 import ShopContextProvider from '../../Context/ShopContext'
 import './productDisplay.css'
-import { use } from 'react'
+import isLoggedIn from '../../utils/isLoggedIn'
+import Error from '../Alert/Error'
+import Success from '../Alert/Success'
 
 const ProductDisplay = (props) => {
     const {product} = props;
     const {addToCart} = useContext(ShopContext);
+    const isLoggedIN = isLoggedIn();
+    const [showError, setShowError] = useState(false);
+    const [showSuccess, setShowSuccess] = useState(false);
+    const handleAddToCart = () => {
+        if (!isLoggedIn()) {
+            setShowError(false); // Reset the state
+            setTimeout(() => {
+                setShowError(true); // Set the state to true after a short delay
+            }, 0);
+        } else {
+            // Add to cart logic
+            addToCart(product.id);
+            setShowSuccess(true); 
+
+        }
+    };
   return (
     <div className='constainer p-4 lg:p-16 w-screen h-[120vh] lg:h-screen flex flex-col lg:flex-row justify-between'>
+        
         <div className="left w-full  lg:w-1/2 flex gap-2 justify-between lg:justify-normal">
             <div className="img-list flex flex-col gap-8 lg:gap-2">
                 <img src={product.image} alt="" />
@@ -52,8 +72,9 @@ const ProductDisplay = (props) => {
                     <div className="size-item">XL</div>
                 </div>
             </div>
-            <button onClick={()=>{addToCart(product.id)}} className='px-6 py-2 rounded-xl border-2 border-red-500 w-1/2 lg:w-1/5 hover:bg-red-500 hover:text-white transition-all'>ADD TO CART</button>
-
+            {showSuccess ? <Link to='/cart'><button className='px-6 py-2 rounded-xl border-2 border-green-500 w-1/2 lg:w-1/5 hover:bg-green-500 hover:text-white transition-all'>View Cart</button></Link> : <button onClick={()=>{handleAddToCart()}} className='px-6 py-2 rounded-xl border-2 border-red-500 w-1/2 lg:w-1/5 hover:bg-red-500 hover:text-white transition-all'>ADD TO CART</button>}
+            {showError ? <Error message="Please login to add to cart" /> : <></>}
+            {showSuccess ? <Success message="Product added to cart" /> : <></>}
         </div>
     </div>
   )
